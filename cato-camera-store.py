c646865ed102a7ab7cb4c06922fae6b04ea45716
Alex Street <alexstreetdev>
2018-11-09 23:30:30 +0000

    try cmd param

--- cato-camera-store.py
+++ cato-camera-store.py
@@ -1,18 +1,19 @@
 from flask import Flask, request, send_file, make_response
 from flask_restful import Api, Resource, abort
 import os.path
+import sys
 
 app = Flask(__name__)
 # removes standard 404 flask message
 app.config["ERROR_404_HELP"]=False
-app.config["UPLOAD_FOLDER"]="/app/cato-camera-store/colin"
+app.config["UPLOAD_FOLDER"]="/app/cato-camera-store"
 api = Api(app)
 
 class MovementImage(Resource):
     def post(self, fname):
         file = request.files['media']
         if file:
-            fpath = os.path.join(app.config['UPLOAD_FOLDER'], fname)
+            fpath = os.path.join(app.config['UPLOAD_FOLDER'],app.config['FILEPATH'], fname)
             print(fpath)
             file.save(fpath)
             msg = "/movement/" + fname + ":" + fpath
@@ -30,4 +31,5 @@ class MovementImage(Resource):
 api.add_resource(MovementImage, '/movement/<string:fname>')
 
 if __name__ == '__main__':
+	app.config['FILEPATH']=sys.argv[1]
     app.run(host="0.0.0.0", port=5001)
